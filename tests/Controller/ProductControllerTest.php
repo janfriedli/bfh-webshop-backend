304594ca7c2e8b0a501e933cf5ae975045a10725
Fribim <jan.friedli@gmx.ch>
2018-09-29 16:43:29 +0200

    added a correct push test

--- tests/Controller/ProductControllerTest.php
+++ tests/Controller/ProductControllerTest.php
@@ -36,4 +36,34 @@ class ProductControllerTest extends WebTestCase
         }
         $this->assertStatusCode(200, $client);
     }
+
+    /**
+     * POST a product
+     */
+    public function testPostProduct()
+    {
+        $this->loadFixtures();
+        $client = $this->makeClient();
+        $product = new \App\Entity\Product();
+        $product->setDescription('a description');
+        $productJson = '{
+            "title": "testTitle",
+            "description": "testDescription"
+        }';
+
+        $client->request(
+            'POST',
+            '/v1/product',
+            [],
+            [],
+            array('CONTENT_TYPE' => 'application/json'),
+            $productJson
+        );
+        $this->assertStatusCode(201, $client);
+
+        $product = json_decode($client->getResponse()->getContent());
+        $this->assertEquals(1, $product->id);
+        $this->assertEquals('testTitle', $product->title);
+        $this->assertEquals('testDescription', $product->description);
+    }
 }
