7a69aeed0f505c57bdf30246a71c9a8a57acbca2
Fribim <jan.friedli@gmx.ch>
2018-10-01 07:29:32 +0200

    adjust migration to maria

--- src/Migrations/Version20180921120125.php
+++ src/Migrations/Version20180921120125.php
@@ -12,17 +12,11 @@ final class Version20180921120125 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
-
         $this->addSql('CREATE TABLE product (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL)');
     }
 
     public function down(Schema $schema) : void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
-
         $this->addSql('DROP TABLE product');
     }
 }
