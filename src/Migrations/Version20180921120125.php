08c7dabee2769200da57dda1b342b9c69887b723
Fribim <jan.friedli@gmx.ch>
2018-10-01 07:35:00 +0200

    use correct auto increment for maria db

--- src/Migrations/Version20180921120125.php
+++ src/Migrations/Version20180921120125.php
@@ -12,7 +12,7 @@ final class Version20180921120125 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
-        $this->addSql('CREATE TABLE product (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL)');
+        $this->addSql('CREATE TABLE product (id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL)');
     }
 
     public function down(Schema $schema) : void
