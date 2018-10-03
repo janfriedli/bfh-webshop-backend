9540bd54ad984e6b14ab3f2ba18212178a89780f
Fribim <jan.friedli@gmx.ch>
2018-10-03 13:44:41 +0200

    testing order deletion

--- tests/Controller/StoreOrderControllerTest.php
+++ tests/Controller/StoreOrderControllerTest.php
@@ -280,39 +280,39 @@ class StoreOrderControllerTest extends WebTestCase
         $this->assertEquals('country', $error[3]->property_path);
         $this->assertEquals('This value should not be blank.', $error[3]->message);
     }
-//
-//    /**
-//     * DELETE a storeOrder
-//     */
-//    public function testDeleteStoreOrder()
-//    {
-//        $this->loadFixtures([
-//            'App\Fixture\Test\StoreOrderFixture'
-//        ]);
-//        $client = $this->makeClient();
-//        $client->request(
-//            'DELETE',
-//            '/v1/order/1',
-//            [],
-//            [],
-//            array('CONTENT_TYPE' => 'application/json'),
-//            null
-//        );
-//        $this->assertStatusCode(204, $client);
-//        $this->assertTrue(
-//            $client->getResponse()->headers->contains(
-//                'Allow',
-//                'GET, PUT, DELETE'
-//            )
-//        );
-//
-//        $client->request('GET', '/v1/order');
-//        $storeOrders = json_decode($client->getResponse()->getContent());
-//        $this->assertEquals(count($storeOrders), 19);
-//        $this->assertEquals(2, $storeOrders[0]->id);
-//    }
-//
-//
+
+    /**
+     * DELETE a storeOrder
+     */
+    public function testDeleteStoreOrder()
+    {
+        $this->loadFixtures([
+            'App\Fixture\Test\StoreOrderFixture'
+        ]);
+        $client = $this->makeClient();
+        $client->request(
+            'DELETE',
+            '/v1/order/1',
+            [],
+            [],
+            array('CONTENT_TYPE' => 'application/json'),
+            null
+        );
+        $this->assertStatusCode(204, $client);
+        $this->assertTrue(
+            $client->getResponse()->headers->contains(
+                'Allow',
+                'GET, PUT, DELETE'
+            )
+        );
+
+        $client->request('GET', '/v1/order');
+        $storeOrders = json_decode($client->getResponse()->getContent());
+        $this->assertEquals(count($storeOrders), 1);
+        $this->assertEquals(2, $storeOrders[0]->id);
+    }
+
+
     /**
      * POST a storeOrder
      * @param string $body
