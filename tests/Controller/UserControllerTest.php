9da5c5f6d644cc2c076e83097c6e6aff322a0e16
Elias Summermatter <elias.summermatter@seccom.ch>
2018-12-19 14:17:28 +0100

    Added tests for user registration

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -79,4 +79,50 @@ class UserControllerTest extends WebTestCase
         $this->assertEquals($response->message,"Username is already taken!");
         $this->assertStatusCode(400, $this->client);
     }
+
+    /**
+     * POST short input
+     */
+    public function testShortInput()
+    {
+        $this->loadFixtures([
+            'App\Fixture\Test\UserFixtures'
+        ]); //inits an empty db
+        $userJson = '{ "username" : "1", "password" : "1", "register_token" : "secret"} ';
+        $this->client->request(
+            'POST',
+            '/v1/register',
+            [],
+            [],
+            array('CONTENT_TYPE' => 'application/json'),
+            $userJson
+        );
+        $response = json_decode($this->client->getResponse()->getContent());
+        $this->assertEquals($response[0]->message,"Your username must be at least 5 characters long");
+        $this->assertEquals($response[1]->message,"Your password must be at least 8 characters long");
+        $this->assertStatusCode(400, $this->client);
+    }
+
+    /**
+     * POST long input
+     */
+    public function testLongInput()
+    {
+        $this->loadFixtures([
+            'App\Fixture\Test\UserFixtures'
+        ]); //inits an empty db
+        $userJson = '{ "username" : "1sadasdasdasdsadsadsaddsaasdsaasdasdasdasdasdassaddsadsadsadsa", "password" : "1sadasdasdasdsadsadsaddsaasdsaasdasdasdasdasdassaddsadsadsadsaasdsadasdsadsadasdsa", "register_token" : "secret"} ';
+        $this->client->request(
+            'POST',
+            '/v1/register',
+            [],
+            [],
+            array('CONTENT_TYPE' => 'application/json'),
+            $userJson
+        );
+        $response = json_decode($this->client->getResponse()->getContent());
+        $this->assertEquals($response[0]->message,"Your username cannot be longer than 25 characters");
+        $this->assertEquals($response[1]->message,"Your password cannot be longer than 64 characters");
+        $this->assertStatusCode(400, $this->client);
+    }
 }
